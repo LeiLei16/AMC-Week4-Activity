d122bbbb9c71f961abce9344d345848ff06cea0f
CabajarLeonardo <168710786+LeiLei16@users.noreply.github.com>
2025-02-06 13:36:07 +0800

    Add files via upload

---
+++ Activity1 Scroll View/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Image, ScrollView, Text} from 'react-native';
+
+const logo = {
+  uri: 'https://reactnative.dev/img/tiny_logo.png',
+  width: 64,
+  height: 64,
+};
+
+const App = () => (
+  <ScrollView>
+    <Text style={{fontSize: 96}}>Scroll me plz Scroll me plzScroll me plzScroll me plzScroll me plzv Scroll me plzScroll me plzvScroll me plzScroll me plzScroll me plzScroll me plzScroll me plzScroll me plzScroll me plzv</Text>
+  </ScrollView>
+);
+
+export default App;
